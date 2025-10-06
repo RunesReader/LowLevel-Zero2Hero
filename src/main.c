2ce807fead9ba52d6ed71ca9976fac48d41a9417
RunesReader <1000000000g@gmail.com>
2025-10-06 14:00:39 +0300

    Add Open/Create database file logic

--- src/main.c
+++ src/main.c
@@ -22,8 +22,9 @@ int main(int argc, char *argv[]) {
 	bool newfile = false;
 	bool list = false;
 	int c;
+	int dbfd = STATUS_ERROR;
 
-	while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
+	while ((c = getopt(argc, argv, "nf:a:l")) != STATUS_ERROR) {
 		switch (c) {
 			case 'n':
 				newfile = true;
@@ -38,7 +39,7 @@ int main(int argc, char *argv[]) {
 				list = true;
 				break;
 			default:
-				return -1;
+				return STATUS_ERROR;
 
 		}
 	}
@@ -47,6 +48,22 @@ int main(int argc, char *argv[]) {
 		printf("Filepath is a requared argument\n");
 		print_usage(argv);
 
-		return 0;
+		return STATUS_SUCCESS;
 	}
+
+	if (newfile) {
+		dbfd = create_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to create database file\n");
+			return STATUS_ERROR;
+		}
+	} else {
+		dbfd = open_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to open database file\n");
+			return STATUS_ERROR;
+		}
+	}
+
+	return STATUS_SUCCESS;
 }
