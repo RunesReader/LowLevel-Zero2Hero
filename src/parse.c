67a20b79a9d3be9311bc6a1e0feac2e10f5cef34
RunesReader <1000000000g@gmail.com>
2025-10-09 22:00:25 +0300

    Add NULL check to add_employee function

--- src/parse.c
+++ src/parse.c
@@ -131,6 +131,11 @@ void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
 }
 
 int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *addstring) {
+	if (employees == NULL) {
+		printf("Try to dereferencing NULL pointer\n");
+		return STATUS_ERROR;
+	}
+	
 	char *name = strtok(addstring, ",");
 	char *addr = strtok(NULL, ",");
 	char *hours = strtok(NULL, ",");
