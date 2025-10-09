87cbe4f0a739e7bea6da60f09a0fc049eaf49cf1
RunesReader <1000000000g@gmail.com>
2025-10-09 22:10:47 +0300

    Return to previous version of add_employee function

--- src/parse.c
+++ src/parse.c
@@ -130,7 +130,7 @@ void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
 
 }
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *addstring) {
+int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
 	if (employees == NULL) {
 		printf("Try to dereferencing NULL pointer\n");
 		return STATUS_ERROR;
@@ -140,9 +140,9 @@ int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *
 	char *addr = strtok(NULL, ",");
 	char *hours = strtok(NULL, ",");
 
-	strncpy(employees[dbhdr->count-1]->name, name, sizeof(employees[dbhdr->count-1]->name));
-	strncpy(employees[dbhdr->count-1]->address, addr, sizeof(employees[dbhdr->count-1]->address));
-	employees[dbhdr->count-1]->hours = atoi(hours);
+	strncpy(employees[dbhdr->count-1].name, name, sizeof(employees[dbhdr->count-1].name));
+	strncpy(employees[dbhdr->count-1].address, addr, sizeof(employees[dbhdr->count-1].address));
+	employees[dbhdr->count-1].hours = atoi(hours);
 
 	return STATUS_SUCCESS;
 }
