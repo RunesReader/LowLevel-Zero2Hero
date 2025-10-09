2484ecbdcacb6ef18486d025d0bd5b7bd2f01e33
RunesReader <1000000000g@gmail.com>
2025-10-09 21:55:59 +0300

    Change add_employee argument

--- src/parse.c
+++ src/parse.c
@@ -130,14 +130,14 @@ void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
 
 }
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
+int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *addstring) {
 	char *name = strtok(addstring, ",");
 	char *addr = strtok(NULL, ",");
 	char *hours = strtok(NULL, ",");
 
-	strncpy(employees[dbhdr->count-1].name, name, sizeof(employees[dbhdr->count-1].name));
-	strncpy(employees[dbhdr->count-1].address, addr, sizeof(employees[dbhdr->count-1].address));
-	employees[dbhdr->count-1].hours = atoi(hours);
+	strncpy(employees[dbhdr->count-1]->name, name, sizeof(employees[dbhdr->count-1]->name));
+	strncpy(employees[dbhdr->count-1]->address, addr, sizeof(employees[dbhdr->count-1]->address));
+	employees[dbhdr->count-1]->hours = atoi(hours);
 
 	return STATUS_SUCCESS;
 }
